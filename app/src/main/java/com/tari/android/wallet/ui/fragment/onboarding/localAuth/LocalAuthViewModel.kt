b1a0a2434a15c90020a8de6596d8e5a35cd3ba41
ingvar.skogen <ingvar.skogen@tari.com>
2024-02-28 20:29:13 +0100

    Fix the update in the background thread issue

--- app/src/main/java/com/tari/android/wallet/ui/fragment/onboarding/localAuth/LocalAuthViewModel.kt
+++ app/src/main/java/com/tari/android/wallet/ui/fragment/onboarding/localAuth/LocalAuthViewModel.kt
@@ -1,10 +1,12 @@
 package com.tari.android.wallet.ui.fragment.onboarding.localAuth
 
-import androidx.lifecycle.MutableLiveData
 import com.tari.android.wallet.extension.addTo
 import com.tari.android.wallet.infrastructure.backup.BackupManager
 import com.tari.android.wallet.infrastructure.security.biometric.BiometricAuthenticationService
 import com.tari.android.wallet.ui.common.CommonViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 class LocalAuthViewModel : CommonViewModel() {
@@ -15,8 +17,8 @@ class LocalAuthViewModel : CommonViewModel() {
     @Inject
     lateinit var backupManager: BackupManager
 
-    val secureState = MutableLiveData(SecureState(true, false, false))
-
+    private val _secureState = MutableStateFlow(SecureState())
+    val secureState = _secureState.asStateFlow()
 
     init {
         component.inject(this)
@@ -28,16 +30,18 @@ class LocalAuthViewModel : CommonViewModel() {
         }.addTo(compositeDisposable)
     }
 
-    fun updateState() {
-        secureState.value = SecureState(
-            authService.isBiometricAuthAvailable,
-            securityPrefRepository.pinCode != null,
-            securityPrefRepository.biometricsAuth == true
-        )
+    private fun updateState() {
+        _secureState.update {
+            SecureState(
+                biometricsAvailable = authService.isBiometricAuthAvailable,
+                pinCodeSecured = securityPrefRepository.pinCode != null,
+                biometricsSecured = securityPrefRepository.biometricsAuth == true,
+            )
+        }
     }
 
     fun securedWithBiometrics() {
         securityPrefRepository.biometricsAuth = true
-        secureState.value = secureState.value!!.copy(biometricsSecured = true)
+        _secureState.update { it.copy(biometricsSecured = true) }
     }
 }
