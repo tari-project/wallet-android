0dc2dd7e55a10bbfde5ed9456bd9ee5a9a6045b2
Igor Danilčenko <ingvar.skogen@tari.com>
2024-02-29 08:40:17 +0100

    Update min. valid FFI version to 1.0.0-rc.5 (#1057)

--- app/src/main/java/com/tari/android/wallet/application/MigrationManager.kt
+++ app/src/main/java/com/tari/android/wallet/application/MigrationManager.kt
@@ -1,6 +1,7 @@
 package com.tari.android.wallet.application
 
 import androidx.lifecycle.viewModelScope
+import com.tari.android.wallet.BuildConfig
 import com.tari.android.wallet.ui.common.SimpleViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,13 +12,12 @@ import javax.inject.Singleton
 @Singleton
 class MigrationManager @Inject constructor(private val manager: WalletManager) {
 
-    private val minValidVersion = DefaultArtifactVersion("v0.52.0")
     private val simpleViewModel = SimpleViewModel()
 
     fun validateVersion(onValid: () -> Unit, onError: () -> Unit) {
         val walletVersion = getCurrentWalletVersion()
 
-        if (walletVersion.isEmpty() || DefaultArtifactVersion(walletVersion) < minValidVersion) {
+        if (walletVersion.isEmpty() || DefaultArtifactVersion(walletVersion) < DefaultArtifactVersion(BuildConfig.LIB_WALLET_MIN_VALID_VERSION)) {
             simpleViewModel.viewModelScope.launch(Dispatchers.Main) { onError() }
         } else {
             simpleViewModel.viewModelScope.launch(Dispatchers.Main) { onValid() }
