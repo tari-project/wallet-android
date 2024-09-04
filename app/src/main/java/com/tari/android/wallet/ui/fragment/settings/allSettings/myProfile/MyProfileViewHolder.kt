0db17e93080e6e4e8f1fa536b6a62a833cd890da
Igor Danilčenko <ingvar.skogen@tari.com>
2024-09-04 17:48:45 +0200

    Add Yat view to the profile view item in the Settings screen

--- app/src/main/java/com/tari/android/wallet/ui/fragment/settings/allSettings/myProfile/MyProfileViewHolder.kt
+++ app/src/main/java/com/tari/android/wallet/ui/fragment/settings/allSettings/myProfile/MyProfileViewHolder.kt
@@ -4,6 +4,7 @@ import com.tari.android.wallet.databinding.ItemMyProfileBinding
 import com.tari.android.wallet.ui.common.recyclerView.CommonViewHolder
 import com.tari.android.wallet.ui.common.recyclerView.ViewHolderBuilder
 import com.tari.android.wallet.ui.extension.setVisible
+import com.tari.android.wallet.util.DebugConfig
 import com.tari.android.wallet.util.addressFirstEmojis
 import com.tari.android.wallet.util.addressLastEmojis
 import com.tari.android.wallet.util.addressPrefixEmojis
@@ -17,6 +18,8 @@ class MyProfileViewHolder(view: ItemMyProfileBinding) : CommonViewHolder<MyProfi
         ui.emojiIdViewContainer.textViewEmojiPrefix.text = item.address.addressPrefixEmojis()
         ui.emojiIdViewContainer.textViewEmojiFirstPart.text = item.address.addressFirstEmojis()
         ui.emojiIdViewContainer.textViewEmojiLastPart.text = item.address.addressLastEmojis()
+        ui.textViewYat.setVisible(item.yat.isNotBlank() && DebugConfig.isYatEnabled)
+        ui.textViewYat.text = item.yat
         ui.root.setOnClickListener { item.action.invoke() }
     }
 
