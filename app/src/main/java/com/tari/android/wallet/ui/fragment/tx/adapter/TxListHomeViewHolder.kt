b316d00fa4aeff00e41d8b15f355fab0baee4431
Igor Danilčenko <ingvar.skogen@tari.com>
2024-09-09 15:37:33 +0200

    Add texts for Coinbase txs

--- app/src/main/java/com/tari/android/wallet/ui/fragment/tx/adapter/TxListHomeViewHolder.kt
+++ app/src/main/java/com/tari/android/wallet/ui/fragment/tx/adapter/TxListHomeViewHolder.kt
@@ -1,33 +1,10 @@
 package com.tari.android.wallet.ui.fragment.tx.adapter
 
-import com.tari.android.wallet.R
 import com.tari.android.wallet.databinding.ItemHomeTxListBinding
-import com.tari.android.wallet.extension.applyFontStyle
-import com.tari.android.wallet.model.CancelledTx
-import com.tari.android.wallet.model.CompletedTx
-import com.tari.android.wallet.model.PendingInboundTx
-import com.tari.android.wallet.model.PendingOutboundTx
 import com.tari.android.wallet.model.Tx
-import com.tari.android.wallet.model.TxStatus
-import com.tari.android.wallet.ui.common.domain.PaletteManager
-import com.tari.android.wallet.ui.common.recyclerView.CommonViewHolder
 import com.tari.android.wallet.ui.common.recyclerView.ViewHolderBuilder
-import com.tari.android.wallet.ui.component.tari.TariFont
-import com.tari.android.wallet.ui.extension.gone
-import com.tari.android.wallet.ui.extension.string
-import com.tari.android.wallet.ui.extension.visible
-import com.tari.android.wallet.ui.fragment.contactBook.data.contacts.ContactDto
-import com.tari.android.wallet.util.WalletUtil
-import com.tari.android.wallet.util.addressFirstEmojis
-import com.tari.android.wallet.util.addressLastEmojis
-import com.tari.android.wallet.util.addressPrefixEmojis
-import org.joda.time.DateTime
-import org.joda.time.Hours
-import org.joda.time.LocalDate
-import org.joda.time.Minutes
-import java.util.Locale
 
-class TxListHomeViewHolder(view: ItemHomeTxListBinding) : CommonViewHolder<TransactionItem, ItemHomeTxListBinding>(view) {
+class TxListHomeViewHolder(view: ItemHomeTxListBinding) : CommonTxListViewHolder<TransactionItem, ItemHomeTxListBinding>(view) {
 
     private lateinit var tx: Tx
 
@@ -36,150 +13,16 @@ class TxListHomeViewHolder(view: ItemHomeTxListBinding) : CommonViewHolder<Trans
 
         with(item.tx) {
             this@TxListHomeViewHolder.tx = this
-            displayAliasOrEmojiId(this, item.contact)
-            displayAmount(this)
-            displayDate(this)
+            displayAliasOrEmojiId(this, item.contact, ui.participantTextView1, ui.participantTextView2, ui.emojiIdViewContainer)
+            displayAmount(this, ui.amountTextView, ui.amountTextViewRound)
+            displayDate(this, ui.dateTextView)
         }
 
         item.gifViewModel.onNewTx(tx)
     }
 
-    private fun displayAliasOrEmojiId(tx: Tx, contact: ContactDto?) {
-        val txUser = tx.tariContact
-        // display contact name or emoji id
-        when {
-            tx.isCoinbase -> {
-                ui.participantTextView1.visible()
-                ui.participantTextView2.gone()
-                ui.participantTextView1.text = string(R.string.tx_details_coinbase_placeholder)
-                ui.emojiIdViewContainer.root.gone()
-            }
-
-            tx.isOneSided -> {
-                val title = string(R.string.tx_list_someone) + " " + string(R.string.tx_list_paid_you)
-                ui.participantTextView1.visible()
-                ui.participantTextView2.gone()
-                ui.participantTextView1.text = title
-                ui.emojiIdViewContainer.root.gone()
-            }
-
-            contact != null && contact.contactInfo.getAlias().isNotEmpty() || txUser.walletAddress.isUnknownUser() -> {
-                val alias = contact?.contactInfo?.getAlias().orEmpty().ifBlank { itemView.context.getString(R.string.unknown_source) }
-                val fullText = when (tx.direction) {
-                    Tx.Direction.INBOUND -> string(R.string.tx_list_sent_a_payment, alias)
-                    Tx.Direction.OUTBOUND -> string(R.string.tx_list_you_paid_with_alias, alias)
-                }
-                ui.participantTextView1.visible()
-                ui.participantTextView1.text = fullText.applyFontStyle(
-                    itemView.context,
-                    TariFont.AVENIR_LT_STD_LIGHT,
-                    listOf(alias),
-                    TariFont.AVENIR_LT_STD_HEAVY
-                )
-                ui.participantTextView2.gone()
-                ui.emojiIdViewContainer.root.gone()
-            }
-
-            else -> { // display emoji id
-                ui.emojiIdViewContainer.root.visible()
-                ui.emojiIdViewContainer.textViewEmojiPrefix.text = txUser.walletAddress.addressPrefixEmojis()
-                ui.emojiIdViewContainer.textViewEmojiFirstPart.text = txUser.walletAddress.addressFirstEmojis()
-                ui.emojiIdViewContainer.textViewEmojiLastPart.text = txUser.walletAddress.addressLastEmojis()
-
-                when (tx.direction) {
-                    Tx.Direction.INBOUND -> {
-                        ui.participantTextView1.gone()
-                        ui.participantTextView2.visible()
-                        ui.participantTextView2.text = string(R.string.tx_list_paid_you)
-                        // paid you
-                    }
-
-                    Tx.Direction.OUTBOUND -> {
-                        ui.participantTextView1.visible()
-                        ui.participantTextView1.text = string(R.string.tx_list_you_paid)
-                        ui.participantTextView2.gone()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun displayAmount(tx: Tx) {
-        val amount = WalletUtil.amountFormatter.format(tx.amount.tariValue)
-        val context = itemView.context
-        val (amountText, textColor, backgroundColor) = when {
-            tx is CancelledTx -> Triple(
-                amount,
-                PaletteManager.getTextBody(context),
-                PaletteManager.getBackgroundPrimary(context)
-            )
-
-            tx is PendingInboundTx -> Triple(
-                "+$amount",
-                PaletteManager.getYellow(context),
-                PaletteManager.getSecondaryYellow(context)
-            )
-
-            tx is PendingOutboundTx -> Triple(
-                "-$amount",
-                PaletteManager.getYellow(context),
-                PaletteManager.getSecondaryYellow(context)
-            )
-
-            tx is CompletedTx && tx.status == TxStatus.MINED_UNCONFIRMED -> Triple(
-                when (tx.direction) {
-                    Tx.Direction.OUTBOUND -> "-$amount"
-                    Tx.Direction.INBOUND -> "+$amount"
-                },
-                PaletteManager.getYellow(context),
-                PaletteManager.getSecondaryYellow(context)
-            )
-
-            tx.direction == Tx.Direction.INBOUND -> Triple(
-                "+$amount",
-                PaletteManager.getGreen(context),
-                PaletteManager.getSecondaryGreen(context)
-            )
-
-            else -> Triple(
-                "-$amount",
-                PaletteManager.getRed(context),
-                PaletteManager.getSecondaryRed(context)
-            )
-        }
-        ui.amountTextView.text = amountText
-        ui.amountTextView.setTextColor(textColor)
-        ui.amountTextViewRound.updateBack(backColor = backgroundColor)
-        val measure = ui.amountTextView.paint.measureText("0".repeat(ui.amountTextView.text.length))
-        val totalPadding = ui.amountTextView.paddingStart + ui.amountTextView.paddingEnd
-        ui.amountTextView.width = totalPadding + measure.toInt()
-    }
-
-    private fun displayDate(tx: Tx) {
-        val txDateTime = DateTime(tx.timestamp.toLong() * 1000L)
-        val txDate = txDateTime.toLocalDate()
-        val todayDate = LocalDate.now()
-        val yesterdayDate = todayDate.minusDays(1)
-        ui.dateTextView.text = when {
-            txDate.isEqual(todayDate) -> {
-                val minutesSinceTx = Minutes.minutesBetween(txDateTime, DateTime.now()).minutes
-                when {
-                    minutesSinceTx == 0 -> string(R.string.tx_list_now)
-                    minutesSinceTx < 60 -> String.format(string(R.string.tx_list_minutes_ago), minutesSinceTx)
-                    else -> String.format(string(R.string.tx_list_hours_ago), Hours.hoursBetween(txDateTime, DateTime.now()).hours)
-                }
-            }
-
-            txDate.isEqual(yesterdayDate) -> string(R.string.home_tx_list_header_yesterday)
-            else -> txDate.toString(DATE_FORMAT, Locale.ENGLISH)
-        }
-    }
-
     companion object {
         fun getBuilder(): ViewHolderBuilder =
             ViewHolderBuilder(ItemHomeTxListBinding::inflate, TransactionItem::class.java) { TxListHomeViewHolder(it as ItemHomeTxListBinding) }
-
-        // e.g. Wed, Jun 2
-        private const val DATE_FORMAT = "E, MMM d"
     }
 }
--- app/src/main/java/com/tari/android/wallet/ui/fragment/tx/adapter/TxListViewHolder.kt
+++ app/src/main/java/com/tari/android/wallet/ui/fragment/tx/adapter/TxListViewHolder.kt
@@ -7,41 +7,20 @@ import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import com.tari.android.wallet.R
 import com.tari.android.wallet.databinding.ItemTxListBinding
-import com.tari.android.wallet.extension.applyFontStyle
-import com.tari.android.wallet.model.CancelledTx
-import com.tari.android.wallet.model.CompletedTx
-import com.tari.android.wallet.model.PendingInboundTx
-import com.tari.android.wallet.model.PendingOutboundTx
-import com.tari.android.wallet.model.TariWalletAddress
 import com.tari.android.wallet.model.Tx
 import com.tari.android.wallet.model.TxNote
-import com.tari.android.wallet.model.TxStatus
-import com.tari.android.wallet.ui.common.domain.PaletteManager
 import com.tari.android.wallet.ui.common.gyphy.presentation.GifStateConsumer
 import com.tari.android.wallet.ui.common.gyphy.presentation.GlideGifListener
 import com.tari.android.wallet.ui.common.gyphy.repository.GifItem
-import com.tari.android.wallet.ui.common.recyclerView.CommonViewHolder
 import com.tari.android.wallet.ui.common.recyclerView.ViewHolderBuilder
-import com.tari.android.wallet.ui.component.tari.TariFont
 import com.tari.android.wallet.ui.extension.dimen
 import com.tari.android.wallet.ui.extension.gone
 import com.tari.android.wallet.ui.extension.setTopMargin
 import com.tari.android.wallet.ui.extension.setVisible
-import com.tari.android.wallet.ui.extension.string
 import com.tari.android.wallet.ui.extension.visible
-import com.tari.android.wallet.ui.fragment.contactBook.data.contacts.ContactDto
 import com.tari.android.wallet.ui.fragment.tx.details.statusString
-import com.tari.android.wallet.util.WalletUtil
-import com.tari.android.wallet.util.addressFirstEmojis
-import com.tari.android.wallet.util.addressLastEmojis
-import com.tari.android.wallet.util.addressPrefixEmojis
-import org.joda.time.DateTime
-import org.joda.time.Hours
-import org.joda.time.LocalDate
-import org.joda.time.Minutes
-import java.util.Locale
 
-class TxListViewHolder(view: ItemTxListBinding) : CommonViewHolder<TransactionItem, ItemTxListBinding>(view), GifStateConsumer {
+class TxListViewHolder(view: ItemTxListBinding) : CommonTxListViewHolder<TransactionItem, ItemTxListBinding>(view), GifStateConsumer {
 
     private val glide = Glide.with(itemView.context)
     private lateinit var tx: Tx
@@ -51,9 +30,9 @@ class TxListViewHolder(view: ItemTxListBinding) : CommonViewHolder<TransactionIt
 
         with(item.tx) {
             this@TxListViewHolder.tx = this
-            displayAliasOrEmojiId(this, item.contact)
-            displayAmount(this)
-            displayDate(this)
+            displayAliasOrEmojiId(this, item.contact, ui.participantTextView1, ui.participantTextView2, ui.emojiIdViewContainer)
+            displayAmount(this, ui.amountTextView, ui.amountTextViewRound)
+            displayDate(this, ui.dateTextView)
             displayStatus(this)
             displayMessage(this)
         }
@@ -63,144 +42,10 @@ class TxListViewHolder(view: ItemTxListBinding) : CommonViewHolder<TransactionIt
         item.gifViewModel.gifState.observeForever { it.handle(this) }
     }
 
-    private fun displayAliasOrEmojiId(tx: Tx, contact: ContactDto?) {
-        val txUser = tx.tariContact
-        // display contact name or emoji id
-        when {
-            tx.isCoinbase -> {
-                ui.participantTextView1.visible()
-                ui.participantTextView2.gone()
-                ui.emojiIdViewContainer.root.gone()
-                ui.participantTextView1.text = string(R.string.tx_details_coinbase_placeholder)
-            }
-
-            tx.isOneSided -> {
-                val title = string(R.string.tx_list_someone) + " " + string(R.string.tx_list_paid_you)
-                ui.participantTextView1.visible()
-                ui.participantTextView2.gone()
-                ui.emojiIdViewContainer.root.gone()
-                ui.participantTextView1.text = title
-            }
-
-            contact != null && contact.contactInfo.getAlias().isNotEmpty() || txUser.walletAddress.isUnknownUser() -> {
-                val alias = contact?.contactInfo?.getAlias().orEmpty().ifBlank { itemView.context.getString(R.string.unknown_source) }
-                val fullText = when (tx.direction) {
-                    Tx.Direction.INBOUND -> string(R.string.tx_list_sent_a_payment, alias)
-                    Tx.Direction.OUTBOUND -> string(R.string.tx_list_you_paid_with_alias, alias)
-                }
-                ui.participantTextView1.visible()
-                ui.participantTextView1.text = fullText.applyFontStyle(
-                    itemView.context,
-                    TariFont.AVENIR_LT_STD_LIGHT,
-                    listOf(alias),
-                    TariFont.AVENIR_LT_STD_HEAVY
-                )
-                ui.emojiIdViewContainer.root.gone()
-                ui.participantTextView2.gone()
-            }
-
-            else -> { // display emoji id
-                displayEmojiId(txUser.walletAddress)
-
-                when (tx.direction) {
-                    Tx.Direction.INBOUND -> {
-                        ui.participantTextView1.gone()
-                        ui.participantTextView2.visible()
-                        ui.participantTextView2.text = string(R.string.tx_list_paid_you)
-                    }
-
-                    Tx.Direction.OUTBOUND -> {
-                        ui.participantTextView1.visible()
-                        ui.participantTextView1.text = string(R.string.tx_list_you_paid)
-                        ui.participantTextView2.gone()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun displayAmount(tx: Tx) {
-        val amount = WalletUtil.amountFormatter.format(tx.amount.tariValue)
-        val context = itemView.context
-        val (amountText, textColor, backgroundColor) = when {
-            tx is CancelledTx -> Triple(
-                amount,
-                PaletteManager.getTextBody(context),
-                PaletteManager.getBackgroundPrimary(context)
-            )
-
-            tx is PendingInboundTx -> Triple(
-                "+$amount",
-                PaletteManager.getYellow(context),
-                PaletteManager.getSecondaryYellow(context)
-            )
-
-            tx is PendingOutboundTx -> Triple(
-                "-$amount",
-                PaletteManager.getYellow(context),
-                PaletteManager.getSecondaryYellow(context)
-            )
-
-            tx is CompletedTx && tx.status == TxStatus.MINED_UNCONFIRMED -> Triple(
-                when (tx.direction) {
-                    Tx.Direction.OUTBOUND -> "-$amount"
-                    Tx.Direction.INBOUND -> "+$amount"
-                },
-                PaletteManager.getYellow(context),
-                PaletteManager.getSecondaryYellow(context)
-            )
-
-            tx.direction == Tx.Direction.INBOUND -> Triple(
-                "+$amount",
-                PaletteManager.getGreen(context),
-                PaletteManager.getSecondaryGreen(context)
-            )
-
-            else -> Triple(
-                "-$amount",
-                PaletteManager.getRed(context),
-                PaletteManager.getSecondaryRed(context)
-            )
-        }
-        ui.amountTextView.text = amountText
-        ui.amountTextView.setTextColor(textColor)
-        ui.amountTextViewRound.updateBack(backColor = backgroundColor)
-        val measure = ui.amountTextView.paint.measureText("0".repeat(ui.amountTextView.text.length))
-        val totalPadding = ui.amountTextView.paddingStart + ui.amountTextView.paddingEnd
-        ui.amountTextView.width = totalPadding + measure.toInt()
-    }
-
-    private fun displayDate(tx: Tx) {
-        val txDateTime = DateTime(tx.timestamp.toLong() * 1000L)
-        val txDate = txDateTime.toLocalDate()
-        val todayDate = LocalDate.now()
-        val yesterdayDate = todayDate.minusDays(1)
-        ui.dateTextView.text = when {
-            txDate.isEqual(todayDate) -> {
-                val minutesSinceTx = Minutes.minutesBetween(txDateTime, DateTime.now()).minutes
-                when {
-                    minutesSinceTx == 0 -> string(R.string.tx_list_now)
-                    minutesSinceTx < 60 -> String.format(string(R.string.tx_list_minutes_ago), minutesSinceTx)
-                    else -> String.format(string(R.string.tx_list_hours_ago), Hours.hoursBetween(txDateTime, DateTime.now()).hours)
-                }
-            }
-
-            txDate.isEqual(yesterdayDate) -> string(R.string.home_tx_list_header_yesterday)
-            else -> txDate.toString(DATE_FORMAT, Locale.ENGLISH)
-        }
-    }
-
     private fun displayStatus(tx: Tx) {
         val status = tx.statusString(context = itemView.context, item!!.requiredConfirmationCount)
         ui.statusTextView.setVisible(status.isNotEmpty())
         ui.statusTextView.text = status
-    }
-
-    private fun displayEmojiId(address: TariWalletAddress) {
-        ui.emojiIdViewContainer.root.visible()
-        ui.emojiIdViewContainer.textViewEmojiPrefix.text = address.addressPrefixEmojis()
-        ui.emojiIdViewContainer.textViewEmojiFirstPart.text = address.addressFirstEmojis()
-        ui.emojiIdViewContainer.textViewEmojiLastPart.text = address.addressLastEmojis()
     }
 
     private fun displayMessage(tx: Tx) {
@@ -256,8 +101,5 @@ class TxListViewHolder(view: ItemTxListBinding) : CommonViewHolder<TransactionIt
     companion object {
         fun getBuilder(): ViewHolderBuilder =
             ViewHolderBuilder(ItemTxListBinding::inflate, TransactionItem::class.java) { TxListViewHolder(it as ItemTxListBinding) }
-
-        // e.g. Wed, Jun 2
-        private const val DATE_FORMAT = "E, MMM d"
     }
 }
