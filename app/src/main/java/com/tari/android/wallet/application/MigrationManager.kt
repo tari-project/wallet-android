a3abb7bec9b20aea5f6cf0f1165f2f32a6222a57
Igor Danilčenko <ingvar.skogen@tari.com>
2024-09-16 14:13:00 +0200

    Fix the MigrationManager issue with versions comparison, removed unused anymore code

--- app/src/main/java/com/tari/android/wallet/application/MigrationManager.kt
+++ app/src/main/java/com/tari/android/wallet/application/MigrationManager.kt
@@ -2,48 +2,27 @@ package com.tari.android.wallet.application
 
 import com.tari.android.wallet.BuildConfig
 import com.tari.android.wallet.application.walletManager.WalletManager
-import com.tari.android.wallet.data.sharedPrefs.network.NetworkPrefRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion
-import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class MigrationManager @Inject constructor(
     private val walletManager: WalletManager,
-    private val application: TariWalletApplication,
-    private val networkRepository: NetworkPrefRepository,
 ) {
+    suspend fun validateVersion(onValid: () -> Unit, onError: () -> Unit) = withContext(Dispatchers.IO) {
+        val walletVersion = walletManager.getCommsConfig().getLastVersion()
+            .replace("v", "")
+            .takeIf { it.isNotEmpty() }
+            ?.let { DefaultArtifactVersion(it) }
+        val minValidVersion = DefaultArtifactVersion(BuildConfig.LIB_WALLET_MIN_VALID_VERSION.replace("v", ""))
 
-    suspend fun validateVersion(onValid: () -> Unit, onError: () -> Unit) {
-        val walletVersion = getCurrentWalletVersion()
-
-        // FIXME: Temporary migration mechanism. It should be removed when minValidVersion is greater than 1.0.0-rc.8
-        if (walletVersion.isNotEmpty() && DefaultArtifactVersion(walletVersion) < DefaultArtifactVersion(PEER_DB_MIGRATION_MIN_VERSION)) {
-            performPeerDbMigration()
-        }
-
-        if (walletVersion.isEmpty() || DefaultArtifactVersion(walletVersion) < DefaultArtifactVersion(BuildConfig.LIB_WALLET_MIN_VALID_VERSION)) {
+        if (walletVersion == null || walletVersion < minValidVersion) {
             onError()
         } else {
             onValid()
         }
-    }
-
-    private suspend fun getCurrentWalletVersion(): String = withContext(Dispatchers.IO) { walletManager.getCommsConfig().getLastVersion() }
-
-    private suspend fun performPeerDbMigration() = withContext(Dispatchers.IO) {
-        application.applicationInfo.dataDir?.let { appDir ->
-            val peerDbFile = File(appDir, "files/${networkRepository.currentNetwork.network.uriComponent}/data.mdb")
-            if (peerDbFile.exists()) {
-                peerDbFile.delete()
-            }
-        }
-    }
-
-    companion object {
-        const val PEER_DB_MIGRATION_MIN_VERSION = "1.0.0-rc.8"
     }
 }
