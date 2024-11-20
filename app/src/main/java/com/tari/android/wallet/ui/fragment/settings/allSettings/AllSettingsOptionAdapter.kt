cd79c64ba40d5b0ee6113af51055e0f09e1f28b8
Igor Danilčenko <ingvar.skogen@tari.com>
2024-11-20 10:33:03 +0100

    Add a bottom space in the All Settings list screen

--- app/src/main/java/com/tari/android/wallet/ui/fragment/settings/allSettings/AllSettingsOptionAdapter.kt
+++ app/src/main/java/com/tari/android/wallet/ui/fragment/settings/allSettings/AllSettingsOptionAdapter.kt
@@ -3,20 +3,22 @@ package com.tari.android.wallet.ui.fragment.settings.allSettings
 import com.tari.android.wallet.ui.common.recyclerView.CommonAdapter
 import com.tari.android.wallet.ui.common.recyclerView.CommonViewHolderItem
 import com.tari.android.wallet.ui.common.recyclerView.ViewHolderBuilder
+import com.tari.android.wallet.ui.common.recyclerView.viewHolders.SpaceVerticalViewHolder
 import com.tari.android.wallet.ui.fragment.settings.allSettings.backupOptions.SettingsBackupOptionViewHolder
-import com.tari.android.wallet.ui.fragment.settings.allSettings.row.SettingsRowViewHolder
 import com.tari.android.wallet.ui.fragment.settings.allSettings.divider.SettingsDividerViewHolder
 import com.tari.android.wallet.ui.fragment.settings.allSettings.myProfile.MyProfileViewHolder
+import com.tari.android.wallet.ui.fragment.settings.allSettings.row.SettingsRowViewHolder
 import com.tari.android.wallet.ui.fragment.settings.allSettings.title.SettingsTitleViewHolder
 import com.tari.android.wallet.ui.fragment.settings.allSettings.version.SettingsVersionViewHolder
 
-class AllSettingsOptionAdapter: CommonAdapter<CommonViewHolderItem>() {
+class AllSettingsOptionAdapter : CommonAdapter<CommonViewHolderItem>() {
     override var viewHolderBuilders: List<ViewHolderBuilder> = listOf(
         SettingsRowViewHolder.getBuilder(),
         SettingsTitleViewHolder.getBuilder(),
         SettingsDividerViewHolder.getBuilder(),
         SettingsVersionViewHolder.getBuilder(),
         MyProfileViewHolder.getBuilder(),
-        SettingsBackupOptionViewHolder.getBuilder()
+        SettingsBackupOptionViewHolder.getBuilder(),
+        SpaceVerticalViewHolder.getBuilder(),
     )
 }
