2ec090a8fd779e32b8cd6044c4451db46ca3085f
Igor Danilčenko <ingvar.skogen@tari.com>
2025-01-10 17:38:30 +0100

    Fix issue with Enum serialization

--- app/src/main/java/com/tari/android/wallet/data/sharedPrefs/securityStages/DisabledTimestampsDto.kt
+++
@@ -1,5 +0,0 @@
-package com.tari.android.wallet.data.sharedPrefs.securityStages
-
-import java.util.Calendar
-
-class DisabledTimestampsDto(val timestamps: MutableMap<WalletSecurityStage, Calendar> = mutableMapOf())
