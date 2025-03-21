7c06f718fc3659f9b19a2a049a1f930f13ffb6f0
Igor Danilčenko <ingvar.skogen@tari.com>
2025-03-21 17:57:54 +0100

    Add new REST methods for Airdrop profile

--- app/src/main/java/com/tari/android/wallet/data/airdrop/AirdropRetrofitService.kt
+++ app/src/main/java/com/tari/android/wallet/data/airdrop/AirdropRetrofitService.kt
@@ -1,13 +1,61 @@
 package com.tari.android.wallet.data.airdrop
 
+import com.google.gson.annotations.SerializedName
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface AirdropRetrofitService {
 
     @GET("/api/miner/stats")
     suspend fun getMinerStats(): MinerStatsResponse
+
+    //  https://airdrop.tari.com/api/miner/status/e88fa464-ebc6-4c29-8bda-5b17122ac1bf
+    @GET("/api/miner/status/{id}")
+    suspend fun getMinerStatus(@Path("id") id: String): MiningStatusResponse
+
+    @GET("/api/user/details")
+    suspend fun getUserDetails(): UserDetailsResponse
+
+    @GET("/api/user/referral-status")
+    suspend fun getReferralStatus(): ReferralStatusResponse
 }
 
 data class MinerStatsResponse(
     val totalMiners: Int,
 )
+
+data class MiningStatusResponse(
+    val mining: Boolean
+)
+
+data class UserDetailsResponse(
+    @SerializedName("user") val user: User,
+) {
+    data class User(
+        @SerializedName("image_url") val imageUrl: String?,
+        @SerializedName("is_bot") val isBot: Boolean,
+        @SerializedName("twitter_followers") val twitterFollowers: Int,
+        @SerializedName("id") val id: String,
+        @SerializedName("referral_code") val referralCode: String,
+        @SerializedName("yat_user_id") val yatUserId: String?,
+        @SerializedName("display_name") val displayName: String,
+        @SerializedName("name") val name: String,
+        @SerializedName("role") val role: String,
+        @SerializedName("provider") val provider: String,
+        @SerializedName("provider_id") val providerId: String,
+        @SerializedName("profileimageurl") val profileImageUrl: String,
+    )
+}
+
+data class ReferralStatusResponse(
+    @SerializedName("referrals") val referrals: List<Referral>,
+) {
+    data class Referral(
+        @SerializedName("name") val name: String,
+        @SerializedName("photos") val photos: String?,
+        @SerializedName("completed") val completed: Boolean,
+    )
+}
+
+
+
