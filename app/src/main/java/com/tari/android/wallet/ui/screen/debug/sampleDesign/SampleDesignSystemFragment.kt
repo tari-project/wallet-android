dfada3f0337d36b3b5cb2e67b513085b485488ff
Igor Danilčenko <ingvar.skogen@tari.com>
2025-03-22 13:23:59 +0100

    Add Fragment.composeContent extension function

--- app/src/main/java/com/tari/android/wallet/ui/screen/debug/sampleDesign/SampleDesignSystemFragment.kt
+++ app/src/main/java/com/tari/android/wallet/ui/screen/debug/sampleDesign/SampleDesignSystemFragment.kt
@@ -4,23 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
 import com.tari.android.wallet.ui.common.CommonFragment
 import com.tari.android.wallet.ui.compose.TariDesignSystem
-import kotlin.getValue
+import com.tari.android.wallet.util.extension.composeContent
 
 class SampleDesignSystemFragment : CommonFragment<SampleDesignSystemViewModel>() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner))
-            setContent {
-                TariDesignSystem(viewModel.currentTheme) {
-                    SampleDesignSystemScreen()
-                }
-            }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = composeContent {
+        TariDesignSystem(viewModel.currentTheme) {
+            SampleDesignSystemScreen()
         }
     }
 
--- app/src/main/java/com/tari/android/wallet/ui/screen/onboarding/inroduction/IntroductionFragment.kt
+++ app/src/main/java/com/tari/android/wallet/ui/screen/onboarding/inroduction/IntroductionFragment.kt
@@ -6,30 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.viewModels
 import com.tari.android.wallet.ui.compose.TariDesignSystem
 import com.tari.android.wallet.ui.screen.onboarding.activity.OnboardingFlowFragment
 import com.tari.android.wallet.util.extension.collectFlow
-import kotlin.getValue
+import com.tari.android.wallet.util.extension.composeContent
 
 class IntroductionFragment : OnboardingFlowFragment<IntroductionViewModel>() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner))
-            setContent {
-                val state by viewModel.uiState.collectAsState()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = composeContent {
+        val state by viewModel.uiState.collectAsState()
 
-                TariDesignSystem(viewModel.currentTheme) {
-                    IntroductionScreen(
-                        uiState = state,
-                        onImportWalletClick = { viewModel.onWalletRestoreClick() },
-                        onCreateWalletClick = { viewModel.onCreateWalletClick() },
-                    )
-                }
-            }
+        TariDesignSystem(viewModel.currentTheme) {
+            IntroductionScreen(
+                uiState = state,
+                onImportWalletClick = { viewModel.onWalletRestoreClick() },
+                onCreateWalletClick = { viewModel.onCreateWalletClick() },
+            )
         }
     }
 
