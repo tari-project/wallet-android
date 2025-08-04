3c72a5a8d982f8bc1897b2200790f358490e1ad8
Igor Danilčenko <ingvar.skogen@tari.com>
2025-08-04 20:04:19 +0200

    Remove unused step in the send a tx flow

--- app/src/main/java/com/tari/android/wallet/model/TransactionData.kt
+++ app/src/main/java/com/tari/android/wallet/model/TransactionData.kt
@@ -7,11 +7,9 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class TransactionData(
     val recipientContact: Contact,
-    val yat: EmojiId? = null, // always null until Yat is implemented
     val amount: MicroTari,
     val note: String?,
     val feePerGram: MicroTari,
-    val isOneSidePayment: Boolean = true, // it's always true since we have only one side payments allowed
 ) : Parcelable {
     val message: String
         get() = note.orEmpty()
